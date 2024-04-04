30c186636818c033b6ceb04d98ce25e28523dfe4
Tomáš Schuster <schustertomas2002@gmail.com>
2024-04-04 16:48:03 +0200

    Add: lazy loading & transition from blurry to original picture

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -16,16 +16,28 @@ hidden_element.forEach((element) => observer.observe(element));
 
 /*cursor-following blur*/
 const blob = document.getElementById('blob');
-
 document.body.onpointermove = event => {
-    const {clientX, clientY} = event;
+    const { clientX, clientY } = event;
 
     blob.animate({
-        left : `${clientX}px`,
-        top : `${clientY}px`
-    }, {duration: 3000, fill: "forwards"})
-    
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    }, { duration: 3000, fill: "forwards" })
+
 }
 /*blur end*/
 
-
+const blurDivs = document.querySelectorAll('.blur-load')
+blurDivs.forEach(div => {
+    const img = div.querySelector("img")
+    function loaded() {
+        div.classList.add('loaded')
+    }
+    if (img.complete) {
+        loaded()
+    }
+    else {
+        img.addEventListener("load", loaded)
+    }
+}
+)
