3b75314987858b4a0bddb8d8d7b74babaee890ff
Tomáš Schuster <schustertomas2002@gmail.com>
2024-04-05 15:47:27 +0200

    Modify: changed to Masonry layout, added functionality to view photo in full resolution, adjusted gallery view for smaller screens

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -16,8 +16,8 @@ document.body.onpointermove = event => {
     const { clientX, clientY } = event;
 
     blob.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
+        left: `${clientX - 150}px`,
+        top: `${clientY - 150}px`
     }, { duration: 3000, fill: "forwards" })
 
 }
@@ -37,3 +37,29 @@ blurDivs.forEach(div => {
     }
 }
 )
+
+/*enlarding image after click */
+document.addEventListener('DOMContentLoaded', function () {
+    const clickableImages = document.querySelectorAll('.clickable');
+
+    clickableImages.forEach(clickable => {
+        clickable.addEventListener('click', function () {
+            if (window.innerWidth > 430) {// Get the high-resolution image src from the child img element
+                const highResSrc = this.querySelector('img').src;
+
+                // Set the modal image src and display the modal
+                document.getElementById('enlargedImage').src = highResSrc;
+                document.getElementById('modal-backdrop').style.display = "block";
+            }
+            else {
+                // Optionally, prevent any default action if the screen is too small
+                e.preventDefault();
+            }
+        });
+    });
+});
+
+// Function to close the modal
+function closeModal() {
+    document.getElementById('modal-backdrop').style.display = "none";
+}
